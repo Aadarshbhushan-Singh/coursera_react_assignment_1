65c54992c7d039f64e6e554fa4f11442bb1106e6
Aadarshbhushan-Singh <singhaadarsh5050@gmail.com>
2022-02-04 21:20:27 +0530

    Initial Commit

---
+++ src/Components/DishDetailsComponent.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap'
+
+export default class DishDetailsComponent extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    renderDish(dish) {
+        if (dish != null) {
+            return (<>
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </>
+            );
+        }
+        else {
+            return (
+                <div>Not selected anything!</div>
+            );
+
+        }
+    }
+
+    renderComments(dish){
+        if(dish != null){
+            return (<>
+            <h2>Comments</h2>
+                {dish.comments.map((item)=>{
+                    return <div key={item.id}>
+                        <p>{item.comment}</p>
+                        <p>--{item.author}, {new Date(item.date).toDateString()}</p>
+                    </div>
+                })}
+            </>);
+        }else{
+            return(<>
+                    <div></div>
+                </>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <>
+            <div className="container col-12 col-md-5 m-1" >
+                <div className="row">
+                    {this.renderDish(this.props.selectedDish)}  
+                </div>
+            </div>
+            <div className="container col-12 col-md-5 m-1" >
+                <div className="row">
+                    {this.renderComments(this.props.selectedDish)}  
+                </div>
+            </div>
+            </>
+            
+        );
+    }
+
+}
